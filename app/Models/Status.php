5dbe05a8adc8e877333e2d62855b633e2aff7a37
Khurramov <khurramov@MacBook-Pro-Khurramov.local>
2024-09-26 17:00:24 +0500

    MINOR: Defined basic relationships

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
+
+    public function ads(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Ad::class);
+    }
 }
